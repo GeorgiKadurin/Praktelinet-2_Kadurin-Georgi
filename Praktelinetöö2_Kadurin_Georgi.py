7f77b29bbfa77bcf4b1d737054220b8d3faa09f7
opilane <opilane@HP-CZC6297B9X>
2022-12-14 15:38:50 +0200

    Add project files.

---
+++ Praktelinetöö2_Kadurin_Georgi.py
@@ -0,0 +1,32 @@
+﻿#1
+try:
+    a=float(input("Sisesta arv? "))
+    if  a>0:
+         print("Positiivne")
+    if a%2==0:
+        print(f"{a} on paaris")
+    else:
+        print(f"{a} on paaritu")
+except :
+    print("Valid andmetüüp")
+
+
+
+
+
+
+
+#2
+from math import *
+a=float(input("Напишите число 1=> "))
+b=float(input("Напишите число 2=> "))
+v=float(input("Напишите число 3=> "))
+try:
+    if (a,b,v)>0:
+         print("Positiivne")
+    if a,b,c=180:
+
+
+
+except:
+           pass
